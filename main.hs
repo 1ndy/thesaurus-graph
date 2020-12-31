9120e4218840d30d6f5a4afaefc85bf4509fd84f
1ndy <hjdemeyer64@gmail.com>
2020-12-31 15:42:06 -0500

    Initial commit

---
+++ main.hs
@@ -0,0 +1,52 @@
+import Control.Monad
+import System.IO
+
+import Data.List
+
+import Data.List.Split
+import Data.Graph.Types
+import Data.Graph.DGraph
+import Data.Graph.Visualize
+
+data Relation = Synonym | Antonym deriving(Show)
+
+createNode :: [String] -> (String, [(String, Relation)])
+createNode (w:ws) = (w, zip ws (repeat Synonym))
+
+
+createGraph :: [[String]] -> DGraph String Relation
+createGraph wordlists = fromList (map createNode wordlists)
+
+read' :: IO String
+read' =
+    putStr "query> "
+    >> hFlush stdout
+    >> getLine
+
+print' :: String -> IO()
+print' = putStrLn
+
+eval' :: DGraph String Relation -> String -> Int -> [String]
+eval' graph word depth = limitedDFS (depth+1) graph [word]
+    where
+        limitedDFS depth graph words =
+            if depth == 1 then
+                words
+            else
+                limitedDFS (depth-1) graph (nub ((concat (map (reachableAdjacentVertices graph) words))++words))
+
+repl graph = do
+    command <- fmap words read'
+    if length command /= 2 then
+        print' "Error: syntax is <word> <depth>"
+    else
+        print' (show (eval' graph (head command) ((read (last command) :: Int))))
+        >> repl graph
+
+main = do
+    lines <- fmap lines $ readFile "small-thesaurus.csv"
+    let wordlists = map (splitOn ",") lines
+    let wordgraph = createGraph wordlists
+    --plotDGraphPng wordgraph "/mnt/c/Users/editor/Desktop/thesaurustest"
+    repl wordgraph
+    return ()
